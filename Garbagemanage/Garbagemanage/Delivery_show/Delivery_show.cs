8e07cc9a299ef801045bd1af7cc9b18ecc62e8ea
luo <lyjh123abc@163.com>
2023-04-17 20:26:12 +0800

    2023-04-17

--- Garbagemanage/Garbagemanage/Delivery_show/Delivery_show.cs
+++ Garbagemanage/Garbagemanage/Delivery_show/Delivery_show.cs
@@ -9,11 +9,11 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 
 
-namespace GarbageDataManage.Delivery_show
+namespace Garbagemanage.Delivery_show
 {
     public partial class Delivery_show : Form
     {
-        SqlConnection myCon = new SqlConnection("Data Source=49.233.5.44;Initial Catalog=personInfo;Integrated Security=false;Pooling=False;uid=sa;password=BJUTbjut1234;database=CGQDataCenter");
+        // SqlConnection myCon = new SqlConnection("Data Source=49.233.5.44;Initial Catalog=personInfo;Integrated Security=false;Pooling=False;uid=sa;password=BJUTbjut1234;database=CGQDataCenter");
         int cycleIndex = 0;
         //private Plc myPLC = null;
 
@@ -22,12 +22,36 @@ namespace GarbageDataManage.Delivery_show
             InitializeComponent();
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private void Delivery_show_Load(object sender, EventArgs e)
         {
-            myCon.Open();
+            //myCon.Open();
             TimerData.Enabled = true;
         }
 
+        /// <summary>
+        /// 子页自适应
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void changed(Size size)
+        {
+            WindowState = FormWindowState.Normal;
+            SuspendLayout();//挂起布局
+            Size = size;
+            ResumeLayout(true);//恢复布局
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void Delivery_show_Resize(object sender, EventArgs e)
+        {
+            //控件随着窗体改变大小
+            //float newx = this.Width / cc.x;
+            //float newy = this.Height / cc.y;
+            //cc.setControls(newx, newy, this);
+            ////让控件在父控件中居中
+            //cc.CenterCtr(panel2, true, true);
+            Console.WriteLine(e);
+        }
         private void PBoxExit_Click(object sender, EventArgs e)
         {
             Close();
@@ -41,141 +65,141 @@ namespace GarbageDataManage.Delivery_show
         private void TimerData_Tick(object sender, EventArgs e)
         {
             //从数据库中获取站点垃圾桶数据
-            SqlDataAdapter mySDA1 = new SqlDataAdapter("select * from garbage1 order by 投放时间 desc", myCon);
-            DataSet ds = new DataSet();
+            //SqlDataAdapter mySDA1 = new SqlDataAdapter("select * from garbage1 order by 投放时间 desc", myCon);
+            //DataSet ds = new DataSet();
             //DataTable myDT1 = new DataTable();
-            mySDA1.Fill(ds);
+            //mySDA1.Fill(ds);
             //int a = 0;
 
 
-            try
-            {
-                object[] arr1 = ds.Tables[0].Rows[0].ItemArray;
-                LabelName1.Text = Convert.ToString(arr1[0]);
-                LabelType1.Text = Convert.ToString(arr1[1]);
-                LabelWeight1.Text = Convert.ToString(arr1[2]) + "kg";
-                LabelTime1.Text = Convert.ToString(arr1[3]);
-                string sdd = Convert.ToString(arr1[1]);
-                if (sdd == "厨余垃圾      ")
-                {
-                    LabelFoodRate.Text = Convert.ToString(arr1[4]) + "%";
-                    LabelFoodWeight.Text = Convert.ToString(arr1[5]) + "kg";
-                    LabelFoodAmount.Text = Convert.ToString(arr1[6]) + "次";
-                }
-                else
-                {
-                    LabelResidualRate.Text = Convert.ToString(arr1[4]) + "%";
-                    LabelResidualWeight.Text = Convert.ToString(arr1[5]) + "kg";
-                    LabelResidualAmount.Text = Convert.ToString(arr1[6]) + "次";
-                }
-                if (LabelName1.Text == "王小五       ")
-                {
-                    PBoxUser1.Image = Garbagemanage.Properties.Resources.王小五;
-                }
-                else
-                {
-                    PBoxUser1.Image = Garbagemanage.Properties.Resources.张大强;
-                }
-                if (LabelType1.Text == "厨余垃圾      ")
-                {
-                    PBoxType1.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
-                }
-                else
-                {
-                    PBoxType1.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
-                }
-            }
-            catch
-            {
+            //try
+            //{
+            //    object[] arr1 = ds.Tables[0].Rows[0].ItemArray;
+            //    LabelName1.Text = Convert.ToString(arr1[0]);
+            //    LabelType1.Text = Convert.ToString(arr1[1]);
+            //    LabelWeight1.Text = Convert.ToString(arr1[2]) + "kg";
+            //    LabelTime1.Text = Convert.ToString(arr1[3]);
+            //    string sdd = Convert.ToString(arr1[1]);
+            //    if (sdd == "厨余垃圾      ")
+            //    {
+            //        LabelFoodRate.Text = Convert.ToString(arr1[4]) + "%";
+            //        LabelFoodWeight.Text = Convert.ToString(arr1[5]) + "kg";
+            //        LabelFoodAmount.Text = Convert.ToString(arr1[6]) + "次";
+            //    }
+            //    else
+            //    {
+            //        LabelResidualRate.Text = Convert.ToString(arr1[4]) + "%";
+            //        LabelResidualWeight.Text = Convert.ToString(arr1[5]) + "kg";
+            //        LabelResidualAmount.Text = Convert.ToString(arr1[6]) + "次";
+            //    }
+            //    if (LabelName1.Text == "王小五       ")
+            //    {
+            //        PBoxUser1.Image = Garbagemanage.Properties.Resources.王小五;
+            //    }
+            //    else
+            //    {
+            //        PBoxUser1.Image = Garbagemanage.Properties.Resources.张大强;
+            //    }
+            //    if (LabelType1.Text == "厨余垃圾      ")
+            //    {
+            //        PBoxType1.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
+            //    }
+            //    else
+            //    {
+            //        PBoxType1.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
+            //    }
+            //}
+            //catch
+            //{
 
-            }
-            try
-            {
-                object[] arr2 = ds.Tables[0].Rows[1].ItemArray;
-                LabelName2.Text = Convert.ToString(arr2[0]);
-                LabelType2.Text = Convert.ToString(arr2[1]);
-                LabelWeight2.Text = Convert.ToString(arr2[2]) + "kg";
-                LabelTime2.Text = Convert.ToString(arr2[3]);
+            //}
+            //try
+            //{
+            //    object[] arr2 = ds.Tables[0].Rows[1].ItemArray;
+            //    LabelName2.Text = Convert.ToString(arr2[0]);
+            //    LabelType2.Text = Convert.ToString(arr2[1]);
+            //    LabelWeight2.Text = Convert.ToString(arr2[2]) + "kg";
+            //    LabelTime2.Text = Convert.ToString(arr2[3]);
 
-                if (LabelName2.Text == "王小五       ")
-                {
-                    PBoxUser2.Image = Garbagemanage.Properties.Resources.王小五;
-                }
-                else
-                {
-                    PBoxUser2.Image = Garbagemanage.Properties.Resources.张大强;
-                }
-                if (LabelType2.Text == "厨余垃圾      ")
-                {
-                    PBoxType2.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
-                }
-                else
-                {
-                    PBoxType2.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
-                }
-            }
-            catch
-            {
+            //    if (LabelName2.Text == "王小五       ")
+            //    {
+            //        PBoxUser2.Image = Garbagemanage.Properties.Resources.王小五;
+            //    }
+            //    else
+            //    {
+            //        PBoxUser2.Image = Garbagemanage.Properties.Resources.张大强;
+            //    }
+            //    if (LabelType2.Text == "厨余垃圾      ")
+            //    {
+            //        PBoxType2.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
+            //    }
+            //    else
+            //    {
+            //        PBoxType2.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
+            //    }
+            //}
+            //catch
+            //{
 
-            }
-            try
-            {
-                object[] arr3 = ds.Tables[0].Rows[2].ItemArray;
-                LabelName3.Text = Convert.ToString(arr3[0]);
-                LabelType3.Text = Convert.ToString(arr3[1]);
-                LabelWeight3.Text = Convert.ToString(arr3[2]) + "kg";
-                LabelTime3.Text = Convert.ToString(arr3[3]);
+            //}
+            //try
+            //{
+            //    object[] arr3 = ds.Tables[0].Rows[2].ItemArray;
+            //    LabelName3.Text = Convert.ToString(arr3[0]);
+            //    LabelType3.Text = Convert.ToString(arr3[1]);
+            //    LabelWeight3.Text = Convert.ToString(arr3[2]) + "kg";
+            //    LabelTime3.Text = Convert.ToString(arr3[3]);
 
-                if (LabelName3.Text == "王小五       ")
-                {
-                    PBoxUser3.Image = Garbagemanage.Properties.Resources.王小五;
-                }
-                else
-                {
-                    PBoxUser3.Image = Garbagemanage.Properties.Resources.张大强;
-                }
-                if (LabelType3.Text == "厨余垃圾      ")
-                {
-                    PBoxType3.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
-                }
-                else
-                {
-                    PBoxType3.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
-                }
-            }
-            catch
-            {
+            //    if (LabelName3.Text == "王小五       ")
+            //    {
+            //        PBoxUser3.Image = Garbagemanage.Properties.Resources.王小五;
+            //    }
+            //    else
+            //    {
+            //        PBoxUser3.Image = Garbagemanage.Properties.Resources.张大强;
+            //    }
+            //    if (LabelType3.Text == "厨余垃圾      ")
+            //    {
+            //        PBoxType3.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
+            //    }
+            //    else
+            //    {
+            //        PBoxType3.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
+            //    }
+            //}
+            //catch
+            //{
 
-            }
-            try
-            {
-                object[] arr4 = ds.Tables[0].Rows[3].ItemArray;
-                LabelName4.Text = Convert.ToString(arr4[0]);
-                LabelType4.Text = Convert.ToString(arr4[1]);
-                LabelWeight4.Text = Convert.ToString(arr4[2]) + "kg";
-                LabelTime4.Text = Convert.ToString(arr4[3]);
+            //}
+            //try
+            //{
+            //    object[] arr4 = ds.Tables[0].Rows[3].ItemArray;
+            //    LabelName4.Text = Convert.ToString(arr4[0]);
+            //    LabelType4.Text = Convert.ToString(arr4[1]);
+            //    LabelWeight4.Text = Convert.ToString(arr4[2]) + "kg";
+            //    LabelTime4.Text = Convert.ToString(arr4[3]);
 
-                if (LabelName4.Text == "王小五       ")
-                {
-                    PBoxUser4.Image = Garbagemanage.Properties.Resources.王小五;
-                }
-                else
-                {
-                    PBoxUser4.Image = Garbagemanage.Properties.Resources.张大强;
-                }
-                if (LabelType4.Text == "厨余垃圾      ")
-                {
-                    PBoxType4.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
-                }
-                else
-                {
-                    PBoxType4.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
-                }
-            }
-            catch
-            {
+            //    if (LabelName4.Text == "王小五       ")
+            //    {
+            //        PBoxUser4.Image = Garbagemanage.Properties.Resources.王小五;
+            //    }
+            //    else
+            //    {
+            //        PBoxUser4.Image = Garbagemanage.Properties.Resources.张大强;
+            //    }
+            //    if (LabelType4.Text == "厨余垃圾      ")
+            //    {
+            //        PBoxType4.Image = Garbagemanage.Properties.Resources.厨余垃圾图标;
+            //    }
+            //    else
+            //    {
+            //        PBoxType4.Image = Garbagemanage.Properties.Resources.其他垃圾图标;
+            //    }
+            //}
+            //catch
+            //{
 
-            }
+            //}
 
 
         }
